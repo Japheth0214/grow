7600b0b0ec0dfd6176a116ce2bc9841bf53201c2
Japheth0214 <japarcillians@gmail,com>
2024-03-15 16:54:36 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('growButton').addEventListener('click', function() {
+  var button = document.getElementById('growButton');
+  
+  // Double the size
+  button.style.transform = 'scale(2)';
+  
+  // Generate a random color
+  var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  
+  // Change button color
+  button.style.backgroundColor = randomColor;
+  
+  // Reset size after a short delay
+  setTimeout(function() {
+    button.style.transform = 'scale(1)';
+  }, 300);
+});
